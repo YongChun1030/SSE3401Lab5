6aa0c6e4fb22e968bd504bbdd1f0731a6e539a4e
YongChun1030 <yongchun021030@gmail.com>
2023-05-12 01:05:19 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:lab5/pages/choose_location.dart';
+import 'package:lab5/pages/home.dart';
+import 'package:lab5/pages/loading.dart';
+
+void main() {
+  runApp(MaterialApp (
+    initialRoute: '/',
+
+    routes: {
+      '/': (context) => Loading(),
+      '/home':(context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
+}
+
